7758eb1869cdbefd27ea0f634448198cea7ebbf7
aakiryukhin <aakiryukhin@yandex-team.ru>
2024-10-19 13:39:41 +0300

    19 october

--- Saturday10/app/src/main/java/ru/andrewkir/saturday10/MainActivity.kt
+++ Saturday10/app/src/main/java/ru/andrewkir/saturday10/MainActivity.kt
@@ -10,11 +10,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import ru.andrewkir.saturday10.components.EnterUsername
 import ru.andrewkir.saturday10.components.GoodsCard
 import ru.andrewkir.saturday10.models.GoodsItemModel
 import ru.andrewkir.saturday10.ui.theme.Saturday10Theme
@@ -30,16 +35,49 @@ class MainActivity : ComponentActivity() {
       Saturday10Theme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
           Column(modifier = Modifier.padding(innerPadding)) {
-            MainContent()
+            EnterNameWithGoods()
+//            EnterUsername()
+//            GoodsExampleContent()
           }
         }
+      }
+    }
+  }
+
+  override fun onDestroy() {
+    Log.d("MYTAG", "destroy")
+    super.onDestroy()
+  }
+}
+
+@Composable
+fun EnterNameWithGoods() {
+  val items = remember { mutableStateListOf<GoodsItemModel>() }
+  EnterUsername(onButtonClick = { name ->
+    items.add(
+      GoodsItemModel(
+        name = name,
+        stars = 0,
+        price = 0,
+        imageId = R.drawable.ershik
+      )
+    )
+  })
+
+  LazyColumn {
+    items.forEach { item ->
+      item {
+        GoodsCard(
+          Modifier.padding(12.dp),
+          item
+        )
       }
     }
   }
 }
 
 @Composable
-fun MainContent() {
+fun GoodsExampleContent() {
   val cards = listOf(
     GoodsItemModel(
       name = "Ershik",
@@ -72,6 +110,6 @@ fun MainContent() {
 @Composable
 fun GreetingPreview() {
   Saturday10Theme {
-    MainContent()
+    GoodsExampleContent()
   }
 }
